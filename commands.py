cdf17c794b77d09efe3cbe607905ffc8de27c1da
Carlo Acosta <acostacarlom@gmail.com>
2022-09-18 21:36:27 +0800

    Added initializers

---
+++ commands.py
@@ -0,0 +1,5 @@
+from pickle import FALSE
+
+
+def processCommand(cmdName):
+    return FALSE
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-# Instantiate voice recognition
-# Instantiate voice synthesizer
+import speech_recognition as sr
+import helper_functions as helper
 
-# While loop that runs indefinitely getting commands - until exit
+
+def main():
+    recognizer = sr.Recognizer()
+    pyaudio = sr.Microphone.get_pyaudio().PyAudio()
+    microphone = helper.get_default_mic(pyaudio)
+
+    while True:
+        voice_audio = helper.listen(microphone, recognizer)
+        voice_text = helper.translate_voice_to_text(voice_audio, recognizer)
+
+        # Get first word that is picked up
+        # Pass that word to processing command
+        # Pass rest of the sentence as arguments
+
+        # Process commands
+
+
+main()
